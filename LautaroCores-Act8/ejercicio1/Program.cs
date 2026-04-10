e4f73329a5b7a4c136a6544b5b1794e415cdd5a1
Lautaro cores <Lautaro.cores09@gmail.com>
2026-04-10 11:56:14 -0300

    Actividad 8

---
+++ LautaroCores-Act8/ejercicio1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class vectorparalelo
+    {
+        /*1. Cargar un vector de n elementos. imprimir el menor y un mensaje si se repite dentro
+del vector.*/
+
+        private int[] vector;
+
+        public void cargaValores()
+        {
+            string linea;
+            Console.WriteLine("cuantos elementos va a ser el vector");
+                linea = Console.ReadLine();
+               vector = new int[int.Parse(linea)];
+
+           
+            Console.WriteLine("ingrese los valores para el vector");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write("");
+                linea = Console.ReadLine();
+                vector[i] = int.Parse(linea);
+            }
+
+        }
+        public void vectorMenor()
+        {
+            int cont = 0;
+            int menor = vector[0];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (menor > vector[i])
+                {
+                    menor = vector[i];
+                }
+                else if (menor == vector[i])
+                {
+                    cont++;
+                }
+            }
+            if (cont == vector.Length)
+            {
+                Console.WriteLine("el elemento menor es se repite");
+            }
+            else
+            {
+                Console.WriteLine("el elemento menor es: " + menor);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            vectorparalelo vector = new vectorparalelo();
+            vector.cargaValores();
+            vector.vectorMenor();
+            Console.ReadKey();
+        
+        }
+    }
+}
---
+++ LautaroCores-Act8/ejercicio2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class vectorPaises
+    {
+
+        private string[] paises;
+
+        public void ordenarPaises()
+        {
+            string aux;
+            paises = new string[] { "Francia", "Brazil", "Argentina", "España", "Venezuela"};
+            for (int i = 0; i < paises.Length; i++)
+            {
+                for (int j = 0; j < paises.Length - 1 - i; j++)
+                {
+                    if (paises[j].CompareTo(paises[j + 1]) > 0)
+                    {
+                        aux = paises[j];
+                        paises[j] = paises[j + 1];
+                        paises[j+1] = aux;  
+                    }
+                }
+            }
+            for (int i = 0;i < paises.Length; i++)
+            {
+                Console.WriteLine(paises[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            vectorPaises paises = new vectorPaises();
+            paises.ordenarPaises();
+            Console.ReadKey();
+        }
+    }
+}
