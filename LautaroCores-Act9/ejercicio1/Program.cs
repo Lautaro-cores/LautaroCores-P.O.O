ba91a30f7530911ca032feb2a1d21496822af776
Lautaro cores <Lautaro.cores09@gmail.com>
2026-04-17 10:10:37 -0300

    Add files via upload

---
+++ LautaroCores-Act9/ejercicio1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class calificacionAlumno
+    {
+
+        /*1. Se desea desarrollar un programa que permita registrar los nombres y las
+calificaciones de 6 estudiantes. Luego de cargar los datos, se debe mostrar el
+nombre del estudiante con la nota más alta, junto con su nota. Al igual que el
+estudiante con la nota más baja. Informar si hay estudiantes con la misma nota
+máxima o mínima.*/
+
+        private int[] calificacion;
+        private string[]alumno;
+        public void cargarValores() {
+            alumno = new string[3];
+            calificacion = new int[3];
+            Console.WriteLine("ingrese el nombre de los 6 alumnos");
+            for (int i = 0; i < alumno.Length; i++)
+            {
+                Console.Write("");
+                alumno[i] = Console.ReadLine(); ;
+            }
+            string linea;
+
+            for (int i = 0; i < alumno.Length; i++)
+            {
+                Console.WriteLine("ingrese la calificacion de " + alumno[i]);
+                Console.Write("");
+                linea = Console.ReadLine();
+                calificacion[i] = int.Parse(linea);
+            }
+        }
+
+        public void informeNotas()
+        {
+            int alta = calificacion[0];
+            for (int i = 0;i < alumno.Length; i++)
+            {
+                if(alta < calificacion[i])
+                {
+                    alta=calificacion[i];
+                }
+            }
+            Console.WriteLine("los alumnos con la mayor nota son :");
+            for (int i = 0; i<alumno.Length; i++)
+            {
+                if (alta == calificacion[i])
+                {
+        
+                    Console.WriteLine(alumno[i]+ ": "+ calificacion[i]);
+                }
+            }
+            int baja = calificacion[0];
+            for (int i = 0; i < alumno.Length; i++)
+            {
+                if (baja > calificacion[i])
+                {
+                    baja = calificacion[i];
+                }
+            }
+            Console.WriteLine("los alumnos con la menor nota son :");
+            for (int i = 0; i < alumno.Length; i++)
+            {
+                if (baja == calificacion[i])
+                {
+                   
+                    Console.WriteLine(alumno[i] + ": " + calificacion[i]);
+                }
+            }
+
+
+        }
+        
+        
+        
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            calificacionAlumno alumno = new calificacionAlumno();
+            alumno.cargarValores();
+            alumno.informeNotas();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LautaroCores-Act9/ejercicio2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4
+{
+    internal class empresaVentas
+    {
+        /*2. Una empresa registra los nombres de sus 5 vendedores y el total de ventas
+realizadas por cada uno en un mes. Cargar los nombres y ventas en dos
+vectores paralelos, ordenar los datos de mayor a menor según las ventas,
+imprimir la lista ordenada con nombre y monto de la venta, e informar quien fue
+el que menos vendió de los 5 empleados.*/
+
+        private string[] empleado;
+
+        private int[] ventas;
+
+        public void cargaValores()
+        {
+            empleado = new string[5];
+            ventas = new int[5];
+            Console.WriteLine("ingrese el nombre de 5 empleados");
+            for (int i = 0; i < empleado.Length; i++)
+            {
+                Console.Write("");
+                empleado[i] = Console.ReadLine(); ;
+            }
+            string linea;
+
+            for (int i = 0; i < empleado.Length; i++)
+            {
+                Console.WriteLine("ingrese las ventas de " + empleado[i]);
+                Console.Write("");
+                linea = Console.ReadLine();
+                ventas[i] = int.Parse(linea);
+            }
+        }
+
+
+
+        public void ordenarVentas()
+        {
+            int aux;
+            string aux2;
+            for (int i = 0; i < empleado.Length; i++)
+            {
+                for (int j = 0; j < ventas.Length - 1 - i; j++)
+                {
+                    if (ventas[j] < ventas[j + 1])
+                    {
+                        aux = ventas[j];
+                        ventas[j] = ventas[j + 1];
+                        ventas[j + 1] = aux;
+                        aux2 = empleado[j];
+                        empleado[j] = empleado[j + 1];
+                        empleado[j + 1] = aux2;
+                    }
+                }
+            }
+            Console.WriteLine("empleado ordenados por ventas");
+            for (int i = 0; i < ventas.Length; i++)
+            {
+                Console.WriteLine(empleado[i] + ": " + ventas[i]);
+            }
+
+        }
+
+        public void menorVentas()
+        {
+
+            int baja = ventas[0];
+            for (int i = 0; i < empleado.Length; i++)
+            {
+                if (baja > ventas[i])
+                {
+                    baja = ventas[i];
+                }
+            }
+            Console.WriteLine("los empleados con la menores ventas son :");
+            for (int i = 0; i < empleado.Length; i++)
+            {
+                if (baja == ventas[i])
+                {
+
+                    Console.WriteLine(empleado[i] + ": " + ventas[i]);
+                }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            empresaVentas empleados = new empresaVentas();
+            empleados.cargaValores();
+            empleados.ordenarVentas();
+            empleados.menorVentas();
+            Console.ReadKey();
+        }
+    }
+}
