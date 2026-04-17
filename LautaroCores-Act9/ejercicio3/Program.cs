5a5837174a24dbbdac089ff90f4413f888eacb81
Lautaro cores <Lautaro.cores09@gmail.com>
2026-04-17 11:47:09 -0300

    Actividad 9

--- LautaroCores-Act9/ejercicio3/Program.cs
+++ LautaroCores-Act9/ejercicio3/Program.cs
@@ -1,15 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ejercicio3
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+
+        internal class altletasTiempos
         {
+            /*2. Una empresa registra los nombres de sus 5 vendedores y el total de ventas
+    realizadas por cada uno en un mes. Cargar los nombres y ventas en dos
+    vectores paralelos, ordenar los datos de mayor a menor según las ventas,
+    imprimir la lista ordenada con nombre y monto de la venta, e informar quien fue
+    el que menos vendió de los 5 empleados.*/
+
+            private string[] atleta;
+
+            private int[] tiempos;
+
+            public void cargaValores()
+            {
+                atleta = new string[5];
+                tiempos = new int[5];
+                Console.WriteLine("ingrese el nombre de 5 atletas");
+                for (int i = 0; i < atleta.Length; i++)
+                {
+                    Console.Write("");
+                    atleta[i] = Console.ReadLine(); ;
+                }
+                string linea;
+
+                for (int i = 0; i < atleta.Length; i++)
+                {
+                    Console.WriteLine("ingrese el tiempo de " + atleta[i]);
+                    Console.Write("");
+                    linea = Console.ReadLine();
+                    tiempos[i] = int.Parse(linea);
+                }
+            }
+        public void promedioTiempo() {
+        int suma = 0;
+        for (int i = 0;i < tiempos.Length; i++)
+            {
+                suma = suma + tiempos[i];
+            }
+        int promedio = suma / tiempos.Length;
+            Console.WriteLine("el promedio de timepos es "+ promedio);
+        
+        for (int i = 0; i<tiempos.Length ; i++)
+            {
+                if (tiempos[i] > promedio)
+                {
+                    Console.WriteLine("el atleta " + atleta[i] + " supero el promdio de tiempo"); 
+                }
+
+            }
+        }
+        public void tiemposMejores()
+        {
+            int alta = tiempos[0];
+            for (int i = 0; i < atleta.Length; i++)
+            {
+                if (alta < tiempos[i])
+                {
+                    alta = tiempos[i];
+                }
+            }
+            Console.WriteLine("el atleta con el mejor tiempo es :");
+            for (int i = 0; i < atleta.Length; i++)
+            {
+                if (alta == tiempos[i])
+                {
+
+                    Console.WriteLine(atleta[i] + ": " + tiempos[i]);
+                }
+            }
+            int baja = tiempos[0];
+            for (int i = 0; i < atleta.Length; i++)
+            {
+                if (baja > tiempos[i])
+                {
+                    baja = tiempos[i];
+                }
+            }
+            Console.WriteLine("el atleta con el peor tiempo es :");
+            for (int i = 0; i < atleta.Length; i++)
+            {
+                if (baja == tiempos[i])
+                {
+
+                    Console.WriteLine(atleta[i] + ": " + tiempos[i]);
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {   
+            altletasTiempos atleta = new altletasTiempos();
+            atleta.cargaValores();
+            atleta.tiemposMejores();
+            atleta.promedioTiempo();
+            Console.ReadKey();
         }
     }
 }
--- LautaroCores-Act9/ejercicio4/Properties/AssemblyInfo.cs
+++ LautaroCores-Act9/ejercicio4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("ejercicio3")]
+[assembly: AssemblyTitle("ejercicio4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Escuela Confederacion Suiza")]
-[assembly: AssemblyProduct("ejercicio3")]
+[assembly: AssemblyProduct("ejercicio4")]
 [assembly: AssemblyCopyright("Copyright © Escuela Confederacion Suiza 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("09caa18d-4c83-4cc1-9a71-1a5e198116de")]
+[assembly: Guid("1139dddb-4ae0-4f47-807d-62c115c6a01c")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
