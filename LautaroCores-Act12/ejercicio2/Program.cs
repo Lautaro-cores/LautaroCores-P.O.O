7b5d6c4a4307d94662a7b1b44e1219ab16aa2d8c
Lautaro cores <Lautaro.cores09@gmail.com>
2026-05-15 09:20:32 -0300

    Actividad 12

--- LautaroCores-Act12/ejercicio2/Program.cs
+++ LautaroCores-Act12/ejercicio2/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ejercicio2
 {
-    internal class Program
+    internal class CineAsientos
     {
         /*2. Gestión de Complejo de Cine
 Un cine tiene 4 salas con diferentes capacidades de espectadores (la Sala 1 tiene 10
@@ -28,11 +28,108 @@ complejo.*/
         private void CargarMatriz()
         {
             asientos = new int[4][];
+            asientos[0] = new int[10];
+            asientos[1] = new int[15];
+            asientos[2] = new int[8];
+            asientos[3] = new int[12];
         }
 
+        private void VentaEntradas()
+        {
+            int aux = 0;
+            int aux2 = 0;
+            int edad = 0;
+            int entradas = 0;
+            Console.WriteLine("cuantas entradas va a comprar");
+            string linea = Console.ReadLine();
+            entradas = int.Parse(linea);
+            for (int i = 0; i < entradas; i++)
+            {
+                Console.WriteLine("indique en que sala va a estar");
+                linea = Console.ReadLine();
+                aux = int.Parse(linea);
+                Console.WriteLine("indique en que asiento va a estar, hay " + asientos[aux].Length + " asientos ");
+                linea = Console.ReadLine();
+                aux2 = int.Parse(linea);
+                while (aux2 > asientos[aux].Length)
+                {
+                    Console.WriteLine("indique devuelta un asiento disponible ");
+                    linea = Console.ReadLine();
+                    aux2 = int.Parse(linea);
+                }
+                while (asientos[aux][aux2] != 0)
+                {
+                    Console.WriteLine("el asiento se ecuentra ocupado, ingrese otro ");
+                    linea = Console.ReadLine();
+                    aux2 = int.Parse(linea);
+                }
+                Console.WriteLine("ingrese su edad");
+                linea = Console.ReadLine();
+                edad = int.Parse(linea);
+
+                asientos[aux][aux2] = edad;
+            }
+        }
+
+        private void MapaAsientos()
+        {
+            for (int i = 0; i < asientos.Length; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < asientos[i].Length; j++)
+                {
+                    Console.Write(" [ "  + asientos[i][j] + " ] ");
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        private void CalcularMenores()
+        {
+            
+            for (int i = 0; i < asientos.Length; i++)
+            {
+                int menores = 0;
+
+                for (int j = 0; j < asientos[i].Length; j++)
+                {
+                    if (asientos[i][j] < 18 && asientos[i][j] != 0) {
+                    menores++;
+                    } 
+                }
+                Console.WriteLine("en la sale " + i + " hay " + menores + " menores de edad");
+            }
+            
+        }
+
+        private void PromedioEdad ()
+        {
+            int aux = 0;
+            int aux2 = 0;
+            for (int i = 0; i < asientos.Length; i++)
+            {
+                for (int j = 0; j < asientos[i].Length; j++)
+                {
+                    if (asientos[i][j] != 0)
+                    {
+                        aux += asientos[i][j];
+                        aux2++;
+                    }
+                }
+            }
+            int promedio = aux / aux2;
+            Console.WriteLine("el promedio de edad en todo el cine es de " + promedio);
+        }
 
         static void Main(string[] args)
         {
+            CineAsientos cine = new CineAsientos();
+            cine.CargarMatriz();
+            cine.VentaEntradas();
+            cine.MapaAsientos();
+            cine.CalcularMenores();
+            cine.PromedioEdad();
+            Console.ReadKey();
         }
     }
 }
---
+++ LautaroCores-Act12/ejercicio3/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class academiaGatronomia
+    {
+        /*3. Academia de Gastronomía: Recetario Dinámico
+Un chef instructor evalúa a 3 alumnos en un examen final. Cada alumno debe presentar
+una cantidad distinta de platos (uno presenta 2 platos, otro 4 y otro 3).
+● Definir un vector para los nombres de los alumnos.
+● Definir una matriz irregular para cargar el puntaje obtenido (0 a 100) en cada plato
+presentado.
+● Métodos:
+1. Cargar nombres y definir el tamaño de las filas según la cantidad de platos
+de cada alumno.
+2. Cargar los puntajes de cada plato validando que estén entre 0 y 100.
+3. Mostrar el listado de alumnos y el puntaje de cada uno de sus platos.
+4. Calcular el puntaje promedio de cada alumno e informar si está &quot;Aprobado&quot;
+(promedio &gt;= 70) o &quot;Reprobado&quot;.
+5. Determinar quién obtuvo el puntaje individual más alto en un solo plato (el
+valor máximo de la matriz).*/
+
+        private int[][] puntuaciones;
+        private string[] participantes;
+
+        private void Inicializar()
+        {
+            participantes = new string[3];
+            for (int i = 0; i < participantes.Length; i++) { 
+            Console.WriteLine("ingrese el nombre del participante " + (i + 1) );
+            string linea = Console.ReadLine();
+            participantes[i] = linea;
+        }
+            puntuaciones = new int[3][];
+            puntuaciones[0] = new int[2];
+            puntuaciones[1] = new int[4];
+            puntuaciones[2] = new int[3];
+        }
+
+        private void CargaPuntuaciones()
+        {
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                for(int j = 0; j < puntuaciones[i].Length; j++)
+                {
+                    Console.WriteLine("ingrese la puntuacion del plato N° " + (j + 1) +" del participante " + participantes[i] );
+                    string linea = Console.ReadLine();
+                    puntuaciones[i][j] = int.Parse(linea);
+                    while (puntuaciones[i][j] > 100 || puntuaciones[i][j] < 0) {
+                        Console.WriteLine("ingrese una puntuacion del 0 al 100");
+                        linea = Console.ReadLine();
+                        puntuaciones[i][j] = int.Parse(linea);
+
+                    }
+                }
+
+            }
+        }
+        
+        private void ImprimirListado()
+        {
+            Console.WriteLine("Las puntuaciones de cada participante son:");
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                Console.WriteLine(participantes[i] + ":");
+                for (int j = 0; j < puntuaciones[i].Length; j++)
+                {
+                    Console.WriteLine("Plato N°" + (j + 1) + " :" + puntuaciones[i][j]);
+                }
+            }
+        }
+
+        private void PromedioPuntuaciones()
+        {
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                int aux = 0;
+                int promedio = 0;
+                for (int j = 0; j < puntuaciones[i].Length; j++)
+                {
+                    aux += puntuaciones[i][j];
+                }
+                promedio = aux/ puntuaciones[i].Length;
+
+                if (promedio >= 70)
+                {
+                    Console.WriteLine("el participante " + participantes[i] + " esta aprobado");
+                }
+                else
+                {
+                    Console.WriteLine("el participante " + participantes[i] + " esta reprobado");
+                }
+            }
+
+        }
+
+        private void MayorPuntaje()
+        {
+            int mayor = puntuaciones[0][0];
+            int aux = 0;
+            for (int i = 0; i < participantes.Length; i++)
+            { 
+                for (int j = 0; j < puntuaciones[i].Length; j++)
+                {
+                    if (puntuaciones[i][j] > mayor  )
+                    {
+                        mayor = puntuaciones[i][j];
+                        aux = i;
+                    }
+                }
+            }
+            Console.WriteLine("el participante " + participantes[aux] + " obtuvo el puntaje mas alto en un plato con una puntuacion de " + mayor);
+
+        }
+
+        static void Main(string[] args)
+        {
+            academiaGatronomia masterchef = new academiaGatronomia();
+            masterchef.Inicializar();
+            masterchef.CargaPuntuaciones();
+            masterchef.ImprimirListado();
+            masterchef.PromedioPuntuaciones();
+            masterchef.MayorPuntaje();
+            Console.ReadKey();
+        }
+    }
+}
