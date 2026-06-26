ed58b6f603a686c2244664dd933d076b898843c8
Lautaro cores <Lautaro.cores09@gmail.com>
2026-06-26 10:18:41 -0300

    Actividad 20

--- LautaroCores-Act20/ejercicio3/Program.cs
+++ LautaroCores-Act20/ejercicio3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ejercicio3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,111 @@ nombres de zonas y total de visitas.
  Determinar cuál fue la zona más visitada.*/
 namespace ejercicio3
 {
-    internal class Program
+    class Zonas
     {
+        private string nombreZona;
+        private int totalVisitas;
+        public string NombreZona { get { return nombreZona; } set { nombreZona = value; } }
+        public int TotalVisitas { get { return totalVisitas; } set { totalVisitas = value; } }
+
+        public Zonas(string nombre, int visitas)
+        {
+            NombreZona = nombre;
+            TotalVisitas = visitas;
+        }
+    }
+
+    class Arqueologia
+    {
+        private int[,] VisitasSemanales;
+        private int[] VisitasTotales;
+        List<Zonas> ZonaVisitas = new List<Zonas>();
+        public Arqueologia()
+        {
+            VisitasSemanales = new int[3, 4];
+            VisitasTotales = new int[3];
+
+            for (int i = 0; i < VisitasSemanales.GetLength(0); i++)
+            {
+                for (int j = 0; j < VisitasSemanales.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Ingrese el numero de visitas de la zona {i + 1} en la semana {j + 1}");
+                    VisitasSemanales[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public void TotalVisitas()
+        {
+
+            for (int i = 0; i < VisitasSemanales.GetLength(0); i++)
+            {
+                int total = 0;
+                for (int j = 0; j < VisitasSemanales.GetLength(1); j++)
+                {
+                    total += VisitasSemanales[i, j];
+                }
+                VisitasTotales[i] = total;
+            }
+
+            for (int i = 0; i < VisitasTotales.Length; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Las visitas totales de la zona {i + 1} son de {VisitasTotales[i]}");
+            }
+        }
+
+        public void ImprimirTabla()
+        {
+            for (int i = 0; i < VisitasSemanales.GetLength(0); i++)
+            {
+                Console.WriteLine("");
+                Console.Write($"Zona {i + 1}  ");
+                for (int j = 0; j < VisitasSemanales.GetLength(1); j++)
+                {
+                    Console.Write($"{VisitasSemanales[i, j]} ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void AgregarList()
+        {
+            for (int i = 0; i < VisitasTotales.Length; i++)
+            {
+                string NombreZona;
+                Console.WriteLine($"Ingrese el nombre de la zona {i}");
+                NombreZona = Console.ReadLine();
+                Zonas zona = new Zonas(NombreZona, VisitasTotales[i]);
+                ZonaVisitas.Add(zona);
+            }
+        }
+
+        public void MasVisitada()
+        {
+            Zonas ZonaMasVisitada = ZonaVisitas[0];
+            foreach (var zona in ZonaVisitas)
+            {
+                if (zona.TotalVisitas > ZonaMasVisitada.TotalVisitas)
+                {
+                    ZonaMasVisitada = zona;
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"La zona mas visitada es la {ZonaMasVisitada.NombreZona} con un total de {ZonaMasVisitada.TotalVisitas} visitas");
+        }
+
+
+
         static void Main(string[] args)
         {
+            Arqueologia arqueologia = new Arqueologia();
+            arqueologia.ImprimirTabla();
+            arqueologia.TotalVisitas();
+            arqueologia.AgregarList();
+            arqueologia.MasVisitada();
+            Console.ReadKey();
         }
     }
 }
+
