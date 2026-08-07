8e612e600d74005db25d0d68c2ea0e10d91e33f1
Lautaro cores <Lautaro.cores09@gmail.com>
2026-08-07 12:05:23 -0300

    Actividad 23

---
+++ LautaroCores-Act23/ejercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*3-
+Un centro de conservación marina registra de forma dinámica las especies de
+mamíferos marinos divisadas en la costa para su posterior análisis estadístico.
+ Crear la clase MonitoreoCostero que contenga como atributo privado
+una lista de cadenas de texto List&lt;string&gt; especiesDetectadas.
+ Métodos en MonitoreoCostero:
+1. CargarAvistamientos(): Solicitar por teclado nombres de
+especies marinas avistadas (ej: &quot;Ballena Franca&quot;, &quot;Lobo Marino&quot;,
+&quot;Delfín&quot;) y agregarlos a la lista utilizando .Add(). La carga finaliza
+cuando el usuario ingresa la palabra &quot;FIN&quot;.
+2. MostrarReporteOrdenado(): Imprimir la lista de avistamientos
+organizada alfabéticamente de la A a la Z utilizando el método
+.Sort().
+3. MostrarReporteInvertido(): Imprimir la lista organizada de la
+Z a la A combinando .Sort() con el método .Reverse().
+4. BuscarEspecie(): Pedir al operador que ingrese el nombre de un
+animal y, utilizando los métodos de búsqueda de listas, informar si la
+especie fue divisada en la costa durante el día.*/
+namespace ejercicio3
+{
+    internal class MonitoreoCostero
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
